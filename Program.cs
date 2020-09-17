5e7c064818dc3318db6162d7f34fe197cb872252
Ike-G <07iglassbrook@brightoncollege.net>
2020-09-17 13:48:12 +0100

    Deleted WPF files in favour for console app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HangManGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
