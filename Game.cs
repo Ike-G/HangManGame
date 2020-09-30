6e5e9ca92572a0a6720d5963f875f84950a045e7
Harrison <harrisonrigby@icloud.com>
2020-09-30 10:24:01 +0100

    need some varible names, should be good

--- Game.cs
+++ Game.cs
@@ -5,9 +5,45 @@ using System.Runtime.InteropServices;
 
 namespace HangManGame 
 {
-    public partial class Game  
+    public partial class Game
     {
-        // In instantiating an object of this class the game should start
+        public string word {get; set;}
+
+        public static char[] wordArray(word)
+        {
+            
+        }
+        
+        public static char[] guessWord(guessLetter, word, wordArray)
+        {
+            bool gameOver = false;
+           
+            char[] blankArray = word.ToCharArray();
+            //for (int i = 0; i ) 
+            int leftToGuess = wordArray().Length
+            for (int i = 0; i < allowedGuesses; i++) 
+            {
+                if (char[] wordArray[i] = guessLetter)
+                {
+                    char[] blankArray[i] = guessLetter;
+                    leftToGuess = leftToGuess - 1;
+                    
+                }
+                if (char[] wordArray[i] != guessLetter); and (wordArray[i] = " " or wordArray[i] = "_")
+                {
+                    char[] blankArray[i] = "_";
+                    int total += 1;
+                }
+                if (int total = leftToGuess) {
+                    wrongGuesses += 1;
+                }
+                if (wrongGuesses = allowedGuesses)
+                {
+                    gameOver = true;
+                }
+            }
+
+            // In instantiating an object of this class the game should start
         // This file contains all logic after the game starts, and dictates when it ends
         partial class Mode 
         {
